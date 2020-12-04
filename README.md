6fdf383f9281f65d96234d8b6a02762ac5a7a56c
Patrick O'Connor <poc.967@gmail.com>
2020-12-03 23:25:22 -0500

    completed script

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # ps5bot
 
-## Simple bot for purchasing a single PlayStation 5 from Walmart. Was able to reduce time to 20.3 seconds.
+### Simple bot for purchasing a single PlayStation 5 from Walmart. Was able to reduce purchase time to 20.3 seconds. When run, this will repeatedly scrape the walmart website on a loop until the 'Out of Stock' badge is gone. Only then will the purchase script be triggered.
+
+## Usage
+
+### clone the repo and install dependencies
+
+### create a .env and fill out similarly to the env-sample
+
+## Run script
+
+`npm run start`
--- index.js
+++ index.js
@@ -1,36 +1,49 @@
 const { Builder, By, until, Key } = require("selenium-webdriver");
-const { elementLocated } = require("selenium-webdriver/lib/until");
+const axios = require("axios");
+const cheerio = require("cheerio");
 require("dotenv").config();
 
-async function buyPlaystation5() {
-  console.time();
-  const driver = await new Builder()
-    .forBrowser("chrome")
-    .withCapabilities({
-      browserName: "chrome",
-      chromeOptions: {
-        args: [
-          'user-agent="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"',
-        ],
-      },
-    })
-    .build();
-  driver.manage().setTimeouts({ implicit: 100000 });
-  driver.manage().window().maximize();
+buyPlaystation5();
 
-  try {
-    await open_site(driver);
-    await add_to_cart(driver);
-    await checkout(driver);
-    await guest_checkout(driver);
-    await input_delivery_details(driver);
-    await payment_details(driver);
-    await driver.quit();
-    console.timeEnd();
-  } catch (error) {
-    console.log(error);
-    await driver.quit();
-    console.timeEnd();
+async function buyPlaystation5(retries = 0) {
+  if (retries === 0) {
+    console.time();
+  }
+
+  const test = await inStock();
+
+  if (test) {
+    const driver = await new Builder()
+      .forBrowser("chrome")
+      .withCapabilities({
+        browserName: "chrome",
+        chromeOptions: {
+          args: [
+            'user-agent="Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"',
+          ],
+        },
+      })
+      .build();
+    driver.manage().setTimeouts({ implicit: 100000 });
+    driver.manage().window().maximize();
+    try {
+      await open_site(driver);
+      await add_to_cart(driver);
+      await checkout(driver);
+      await guest_checkout(driver);
+      await input_delivery_details(driver);
+      await payment_details(driver);
+      await driver.quit();
+      console.timeEnd();
+      return console.log("Done!");
+    } catch (error) {
+      await driver.quit();
+      console.timeEnd();
+    }
+  } else {
+    retries += 1;
+    buyPlaystation5(retries);
+    console.log(retries);
   }
 }
 
@@ -44,7 +57,7 @@ async function add_to_cart(driver) {
     30000
   );
   await button.click();
-  // await driver.sleep(3000);
+  await driver.sleep(3000);
   console.log("Item added to cart");
 }
 
@@ -53,10 +66,10 @@ async function checkout(driver) {
     until.elementLocated(By.className("checkoutBtn")),
     3000
   );
-  // await driver.sleep(3000);
+  await driver.sleep(3000);
 
   await cart.click();
-  // await driver.sleep(3000);
+  await driver.sleep(3000);
   console.log("clicked checkout");
 }
 
@@ -64,12 +77,12 @@ async function guest_checkout(driver) {
   await driver
     .wait(until.elementLocated(By.className("button--primary")), 30000)
     .click();
-  // await driver.sleep(3000);
+  await driver.sleep(3000);
 
   await driver
     .wait(until.elementLocated(By.className("cxo-continue-btn")), 30000)
     .click();
-  // await driver.sleep(3000);
+  await driver.sleep(3000);
 }
 
 async function input_delivery_details(driver) {
@@ -87,19 +100,18 @@ async function input_delivery_details(driver) {
   await driver.findElement(By.id("city")).sendKeys(process.env.city);
   await driver.findElement(By.className("input-toggle__input")).click();
   await driver.findElement(By.id("postalCode")).clear();
-  // await driver.sleep(3000);
+  await driver.sleep(3000);
   await driver
     .findElement(By.id("postalCode"))
     .sendKeys(process.env.postalCode, Key.ENTER);
-  // await driver.sleep(3000);
-  // await driver.sleep(3000);
+  await driver.sleep(3000);
 }
 
 async function payment_details(driver) {
-  // await driver.sleep(3000);
+  await driver.sleep(3000);
   await driver.findElement(By.id("cvv")).sendKeys(process.env.ccCVV);
   await driver.findElement(By.id("creditCard")).sendKeys(process.env.ccNumber);
-  // await driver.sleep(3000);
+  await driver.sleep(3000);
   await driver
     .findElement(
       By.css(
@@ -109,7 +121,7 @@ async function payment_details(driver) {
       )
     )
     .click();
-  // await driver.sleep(3000);
+  await driver.sleep(3000);
   await driver
     .findElement(
       By.css(
@@ -123,5 +135,26 @@ async function payment_details(driver) {
   await driver.sleep(10000);
 }
 
-// add cheerio script to webscrape and trigger this function when the playstation becomes available
-buyPlaystation5();
+async function inStock() {
+  try {
+    const html = await axios({
+      method: "GET",
+      url: process.env.base_url,
+      headers: {
+        Accept: "*/*",
+        "User-Agent": "PostmanRuntime/7.26.8",
+        "Content-Type": "application/json",
+        "Accept-Encoding": "gzip, deflate, br",
+        Connection: "keep-alive",
+      },
+    });
+    if (html.data) {
+      const $ = cheerio.load(html.data);
+      const isInStock =
+        $(".prod-blitz-copy-message").text() === "Out of stock." ? false : true;
+      return isInStock;
+    }
+  } catch (error) {
+    throw new Error(error.response.statusText, error.response.status);
+  }
+}
